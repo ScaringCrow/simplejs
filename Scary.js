29bd6a99e62d8a8f6771280015bc7bb0f51033f7
Tharun Pandiyan <tharunpandiyanv@gmail.com>
2021-02-12 17:15:02 +0000

    adding ajax and json parser to get the code Signed-off-by: Tharun Pandiyan <tharunpandiyanv@gmail.com>

--- Scary.js
+++ Scary.js
@@ -1,1 +1,8 @@
-const code = JSON.parse("codecontainer.json")
+var jsontp = new XMLHttpRequest();
+jsontp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var code = JSON.parse(this.responseText);
+  }
+};
+jsontp.open("GET", "codecontaiiner.json", true);
+jsontp.send();
