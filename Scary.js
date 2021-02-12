453bd8e1a294b270a309ee5eabf0113a85428d79
Tharun Pandiyan <tharunpandiyanv@gmail.com>
2021-02-12 17:36:10 +0000

    evaluating the code Signed-off-by: Tharun Pandiyan <tharunpandiyanv@gmail.com>

--- Scary.js
+++ Scary.js
@@ -1,7 +1,16 @@
 var jsontp = new XMLHttpRequest();
+function evaluate(func){
+  var func = eval(`(code.${func})`);
+}
 jsontp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var code = JSON.parse(this.responseText);
+    evaluate("out.print");
+    evaluate("out.println");
+    evaluate("elem.new");
+    evaluate("elem.inner");
+    evaluate("elem.outer");
+    evaluate("looks.border.px")
   }
 };
 jsontp.open("GET", "codecontaiiner.json", true);
