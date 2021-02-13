f40b4f9a9b027b1723e0c3e08c292112c51ee7ce
Tharun Pandiyan <tharunpandiyanv@gmail.com>
2021-02-13 16:53:49 +0000

    git problems Signed-off-by: Tharun Pandiyan <tharunpandiyanv@gmail.com>

--- Scary.js
+++ Scary.js
@@ -2,7 +2,7 @@ var jsontp = new XMLHttpRequest();
 function evaluate(func){
   var func = eval(`(code.${func})`);
 }
-jsontp.onreadystatechange = function() {
+jsontp.onreadystatechange = function scary() {
   if (this.readyState == 4 && this.status == 200) {
     var code = JSON.parse(this.responseText);
     evaluate("out.print");
@@ -15,3 +15,4 @@ jsontp.onreadystatechange = function() {
 };
 jsontp.open("GET", "codecontaiiner.json", true);
 jsontp.send();
+export scary;
