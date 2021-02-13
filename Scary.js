38093e561ea21be02b7638941eab769f3f08d121
Tharun <tharunpandiyanv@gmail.com>
2021-02-13 17:19:42 +0000

    Master (#5)
    * getting the right extention for package-lock
    Signed-off-by:  Tharun <tharunpandiyanv@gmail.com>
    
    * dont need
    
    Signed-off-by: Tharun <tharunpandiyanv@gmail.com>
    
    * Signed-off-by: Tharun <tharunpandiyanv@gmail.com>
    
    * upgrading out
    Signed-off-by: Tharun Pandiyan <tharunpandiyanv@gmail.com>
    
    * adding ajax and json parser to get the code
    Signed-off-by: Tharun Pandiyan <tharunpandiyanv@gmail.com>
    
    * evaluating the code
    Signed-off-by: Tharun Pandiyan <tharunpandiyanv@gmail.com>
    
    * commit
    Signed-off-by: Tharun Pandiyan <tharunpandiyanv@gmail.com>
    
    * git problems
    Signed-off-by: Tharun Pandiyan <tharunpandiyanv@gmail.com>
    
    Co-authored-by: deviselvi <deviselvi_2003@yahoo.co.in>

--- Scary.js
+++ Scary.js
@@ -1,12 +1,19 @@
 var jsontp = new XMLHttpRequest();
-function evaluate(func) {
-  eval(`(${func})`)
+function evaluate(func){
+  var func = eval(`(code.${func})`);
 }
 function change() {
-  if (this.readyState == 4 && this.status == 200) {
+if (this.readyState == 4 && this.status == 200) {
     var code = JSON.parse(this.responseText);
+    evaluate("out.print");
+    evaluate("out.println");
+    evaluate("elem.new");
+    evaluate("elem.inner");
+    evaluate("elem.outer");
+    evaluate("looks.border.px")
   }
 }
 jsontp.onreadystatechange = change();
 jsontp.open("GET", "codecontaiiner.json", true);
 jsontp.send();
+export code;
